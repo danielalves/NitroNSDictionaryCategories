b23bb3916ff7960a03dd46ec6167dbada9e69c97
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-07-15 16:00:05 -0300

    Adding a general header that includes all other headers in the pod

---
+++ NitroNSDictionaryCategories/NitroNSDictionaryCategories/NitroNSDictionaryCategories.h
@@ -0,0 +1,15 @@
+//
+//  NitroNSDictionaryCategories.h
+//  NitroNSDictionaryCategories
+//
+//  Created by Daniel L. Alves on 15/7/14.
+//  Copyright (c) 2014 Daniel L. Alves. All rights reserved.
+//
+
+#ifndef NITRO_NSDICTIONARY_CATEGORIES_H
+#define NITRO_NSDICTIONARY_CATEGORIES_H
+
+#import <NitroNSDictionaryCategories/NSDictionary+Parsing_Nitro.h>
+#import <NitroNSDictionaryCategories/NSDictionary+Utils_Nitro.h>
+
+#endif
